ad2ee421e6168646b7acd8c852b61b8290584828
niamatolla <ahansalniamatollah611@gmail.com>
2026-05-11 14:02:56 -0400

    feat(NewRequest): removed absolute positionning and kept same rotation angle

--- client/src/pages/NewRequest.jsx
+++ client/src/pages/NewRequest.jsx
@@ -248,7 +248,7 @@ export default function NewRequest() {
       <img
         src={edge}
         alt=""
-        className="pointer-events-none absolute bottom-0 right-0 hidden w-44 -rotate-90 opacity-90 lg:block"
+        className="pointer-events-none ml-auto mr-0 block w-44 rotate-180 opacity-90"
         draggable="false"
       />
     </div>
