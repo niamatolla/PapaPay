bafb3bd756220323d0a4ab15df2bf4705347db25
niamatolla <ahansalniamatollah611@gmail.com>
2026-05-11 13:56:16 -0400

    feat(NewRequest): added an edge decoration

--- client/src/pages/NewRequest.jsx
+++ client/src/pages/NewRequest.jsx
@@ -248,7 +248,7 @@ export default function NewRequest() {
       <img
         src={edge}
         alt=""
-        className="pointer-events-none absolute right-0 top-1/2 hidden w-44 -translate-y-1/2 rotate-90 opacity-90 lg:block"
+        className="pointer-events-none absolute bottom-0 right-0 hidden w-44 rotate-90 opacity-90 lg:block"
         draggable="false"
       />
     </div>
